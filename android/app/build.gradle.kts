a896338d093f5e9aa035c33a04aee8c2e3423d8b
NunoSid <35347350+NunoSid@users.noreply.github.com>
2026-09-12 23:01:07 +0100

    Bump app to v0.9.0 multi-chat filters

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         applicationId = "pt.nunosid.tgmedialibrary"
         minSdk = 26
         targetSdk = 35
-        versionCode = 9
-        versionName = "0.8.0"
+        versionCode = 10
+        versionName = "0.9.0"
     }
 
     signingConfigs {
