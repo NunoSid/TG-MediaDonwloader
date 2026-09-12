9d26d93619783c2db39794497aecc46ded0e58cb
NunoSid <35347350+NunoSid@users.noreply.github.com>
2026-09-12 16:08:14 +0100

    Add selectable history scopes

--- android/app/src/main/java/pt/nunosid/tgmedialibrary/model/VideoItem.kt
+++ android/app/src/main/java/pt/nunosid/tgmedialibrary/model/VideoItem.kt
@@ -21,6 +21,16 @@ enum class VideoSort {
     NEWEST, OLDEST, LARGEST, SMALLEST, LONGEST, SHORTEST
 }
 
+enum class HistoryScope {
+    LAST_100,
+    LAST_500,
+    LAST_1000,
+    LAST_30_DAYS,
+    LAST_90_DAYS,
+    LAST_YEAR,
+    ALL
+}
+
 data class VideoFilters(
     val query: String = "",
     val chatId: Long? = null,
