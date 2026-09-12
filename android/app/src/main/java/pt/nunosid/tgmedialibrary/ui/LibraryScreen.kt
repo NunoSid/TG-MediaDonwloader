135713bc58873778d0a2f7d8185f138071824e7c
NunoSid <35347350+NunoSid@users.noreply.github.com>
2026-09-12 16:40:11 +0100

    Fix RectangleShape import in media library screen

--- android/app/src/main/java/pt/nunosid/tgmedialibrary/ui/LibraryScreen.kt
+++ android/app/src/main/java/pt/nunosid/tgmedialibrary/ui/LibraryScreen.kt
@@ -7,13 +7,13 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
-import androidx.compose.foundation.shape.RectangleShape
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.drawBehind
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
