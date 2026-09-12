084edf555f6621e499069400b75873be36bbe1cf
NunoSid <35347350+NunoSid@users.noreply.github.com>
2026-09-12 16:38:30 +0100

    Fix RectangleShape import in auth screens

--- android/app/src/main/java/pt/nunosid/tgmedialibrary/ui/RootScreen.kt
+++ android/app/src/main/java/pt/nunosid/tgmedialibrary/ui/RootScreen.kt
@@ -5,12 +5,12 @@ import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.shape.RectangleShape
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.input.VisualTransformation
